07471a67ba03e2ba1c1629e6ee38634ff3c60f87
mama-kod <rosemariejean509@gmail.com>
2025-05-20 13:53:20 -0400

    Updated Login.tsx

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -179,7 +179,7 @@ const Login = () => {
               <div className="flex items-center">
                 <Lock className="h-5 w-5 text-primary-500 mr-2" />
                 <p className="text-sm text-gray-600">
-                  Bwat Sekrè | la gestion des comptes, accorde des prêts et facilite les transactions monétaires ....
+                  Bwat Sekrè | La gestion des comptes, accorde des prêts et facilite les transactions monétaires ....
                 </p>
               </div>
             </div>
