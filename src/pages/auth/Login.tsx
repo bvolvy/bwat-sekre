d5311edc17db3d8543f759fc6180f7348746392a
bvolvy <bazilevolvy@gmail.com>
2025-05-24 00:26:14 -0400

    Update contexts to handle organization-specific data

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -188,7 +188,7 @@ const Login = () => {
               </div>
               <div className="relative flex justify-center text-sm">
                 <span className="px-2 bg-white text-gray-500">
-                 © Bwat Sekrè | 2025
+                 Système de Gestion Bancaire
                 </span>
               </div>
             </div>
